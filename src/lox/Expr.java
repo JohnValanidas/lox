3d73f97573351a32587cc5b8e84a9450a83575a9
John Thomas Shaw Valanidas <john.valanidas@gmail.com>
2024-05-23 00:00:49 -0400

    finsih chapter 5: Representing Code

---
+++ src/lox/Expr.java
@@ -0,0 +1,68 @@
+package lox;
+
+import java.util.List;
+
+abstract class Expr {
+    static class Binary extends Expr {
+        Binary(Expr lhs, Token operator, Expr rhs) {
+            this.lhs = lhs;
+            this.operator = operator;
+            this.rhs = rhs;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitBinaryExpr(this);
+        }
+
+        final Expr lhs;
+        final Token operator;
+        final Expr rhs;
+    }
+    static class Grouping extends Expr {
+        Grouping(Expr expression) {
+            this.expression = expression;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitGroupingExpr(this);
+        }
+
+        final Expr expression;
+    }
+    static class Literal extends Expr {
+        Literal(Object value) {
+            this.value = value;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitLiteralExpr(this);
+        }
+
+        final Object value;
+    }
+    static class Unary extends Expr {
+        Unary(Token operator, Expr rhs) {
+            this.operator = operator;
+            this.rhs = rhs;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitUnaryExpr(this);
+        }
+
+        final Token operator;
+        final Expr rhs;
+    }
+
+    abstract <R> R accept(Visitor<R> visitor);
+    interface Visitor<R> {
+        R visitBinaryExpr(Binary expr);
+        R visitGroupingExpr(Grouping expr);
+        R visitLiteralExpr(Literal expr);
+        R visitUnaryExpr(Unary expr);
+    }
+}
